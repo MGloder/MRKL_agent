470fa0682671d3dd4a8b103c64825c3534237226
Yan Xu <yanxuok@gmail.com>
2024-12-02 15:46:52 +0800

    add unified context

--- src/core/entity/unified_context.py
+++ src/core/entity/unified_context.py
@@ -1,10 +1,21 @@
 import dataclasses
+from typing import List
 
-from core.entity.role import Role
+from core.entity.role import Role, State
 from core.entity.target import Target
 
 
-@dataclasses
 class UnifiedContext:
+    """
+    current_state
+    reference: role, target, interaction_his
+    """
+
+    current_state: State
     role: Role
     target: Target
+    interaction_his: List[str]
+
+    def get_current_state(self) -> State:
+        """Get the current state of the role"""
+        return self.current_state
