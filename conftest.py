1d6759cda8a5c0eab208cb2f1dc2783bcdcba325
Yan Xu <yanxuok@gmail.com>
2024-12-03 14:16:53 +0800

    add conftest

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+# Get the absolute path of the project root directory
+project_root = os.path.dirname(os.path.abspath(__file__))
+
+# Add the project root to Python path
+sys.path.append(project_root)
